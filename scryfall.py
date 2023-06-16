930dc01823bf167c051df3ac8c820e4120ce7be0
OIdGregg <136068230+OIdGregg@users.noreply.github.com>
2023-06-16 14:25:30 -0500

    Delete scryfall.py

--- scryfall.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class Scryfall:
-    API_URL = 'https://api.scryfall.com/cards/search'
-
-    def search(self, query):
-        response = requests.get(self.API_URL, params={'q': query})
-        response.raise_for_status()  # raise exception if invalid response
-        return response.json().get('data', [])
